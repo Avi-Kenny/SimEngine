aeb9052c241c59c7874061aec5dd316d63f6ea70
Avi Kenny <avikenny@uw.edu>
2021-01-31 14:05:46 -0800

    Bug fix

--- R/summary.R
+++ R/summary.R
@@ -705,7 +705,7 @@ summary.simba <- function(sim_obj, ...) {
       }
 
       code_bias_pct <- c(code_bias_pct, paste0(
-        b$name, " = mean(", b$estimate, "-", b$truth, na_1, "/", b$truth, ","
+        b$name, " = mean(", b$estimate, "-", b$truth, na_1, "/abs(", b$truth, "),"
       ))
 
     }
