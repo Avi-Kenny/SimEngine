fd324209093b99269bc44d5f94212e8b35ca79f4
Avi Kenny <avikenny@uw.edu>
2021-08-05 13:10:28 -0700

    Created js_support() function to print info about job schedulers

---
+++ R/js_support.R
@@ -0,0 +1,46 @@
+#' Display information about currently-supported job schedulers
+#'
+#' @description Run this function to display information about job schedulers
+#'     that are currently supported for running \pkg{simba} simulations on a
+#'     cluster computing system (CCS).
+#' @return NULL
+#' @examples
+#' js_support()
+#' @export
+js_support <- function() UseMethod("js_support")
+
+#' @export
+js_support <- function() {
+
+  df <- data.frame(
+    "name" = character(),
+    "js_code" = character(),
+    "developer" = character(),
+    "notes" = character(),
+    stringsAsFactors = FALSE
+  )
+
+  add <- function(df, ls) {
+    df[nrow(df)+1,] <- ls
+    return(df)
+  }
+
+  df <- add(df, list(
+    name = "Slurm workload manager",
+    js_code = "slurm",
+    developer = "SchedMD",
+    notes = ""
+  ))
+  df <- add(df, list(
+    name = "Oracle Grid Engine",
+    js_code = "oge",
+    developer = "Oracle",
+    notes = "Formerly known as 'Sun Grid Engine'"
+  ))
+
+  cat("Job schedulers currently supported by `simba`\n")
+  cat("---------------------------------------------\n")
+
+  return(df)
+
+}
