4a35ce6221974ac06320026e02417e923f5cccc2
Avi Kenny <avikenny@uw.edu>
2022-06-16 11:19:01 -0700

    Bug fix involving functions that have no environment (e.g. `as.integer`)

--- R/run.R
+++ R/run.R
@@ -105,7 +105,7 @@ run.sim_obj <- function(sim, sim_uids=NA) {
     }
     for (obj_name in ls(sim$vars$env)) {
       obj <- get(obj_name, envir=sim$vars$env, inherits=FALSE)
-      if (methods::is(obj,"function")) {
+      if (methods::is(obj,"function") && !is.null(environment(obj))) {
         assign(x="L", value=L, envir=environment(obj))
       }
     }
