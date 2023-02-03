5a9fd10b24080372ff9eed03ec6af0a405d219a2
Avi Kenny <avikenny@uw.edu>
2023-02-03 11:14:05 -0800

    Switched from SimEngine.R to SimEngine-package.R; added imports

---
+++ R/SimEngine-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @importFrom stats runif
+## usethis namespace: end
+NULL
