96e278844e2ff77bcb222d8c8bded22b6d7f6536
Charlie Wolock <cwolock@gmail.com>
2024-03-08 12:09:29 -0500

    update replication code

--- paper/replication_code.R
+++ paper/replication_code.R
@@ -2,7 +2,7 @@
 ##
 ## Avi Kenny and Charles J. Wolock
 ##
-## The code follows the organization of the paper, divided by section. 
+## The code follows the organization of the paper, divided by section.
 
 ## Required packages for replication
 library("SimEngine")
@@ -13,7 +13,104 @@ set.seed(72724)
 
 ## Section 2: Introduction
 
+# Chunk 2.0.1
+# run only if not installed
+# install.packages("SimEngine")
+
+# Chunk 2.1.1
+library(SimEngine)
+sim <- new_sim()
+
+# Chunk 2.2.1
+create_rct_data <- function (num_patients) {
+  df <- data.frame(
+    "patient_id" = integer(),
+    "group" = character(),
+    "outcome" = double(),
+    stringsAsFactors = FALSE
+  )
+  for (i in 1:num_patients) {
+    group <- ifelse(sample(c(0,1), size=1)==1, "treatment", "control")
+    treatment_effect <- ifelse(group=="treatment", -7, 0)
+    outcome <- rnorm(n=1, mean=130, sd=2) + treatment_effect
+    df[i,] <- list(i, group, outcome)
+  }
+  return (df)
+}
+create_rct_data(5)
+
+# Chunk 2.3.1
+est_tx_effect <- function(df, type) {
+  n <- nrow(df)
+  sum_t <- sum(df$outcome * (df$group=="treatment"))
+  sum_c <- sum(df$outcome * (df$group=="control"))
+  if (type=="est1") {
+    true_prob <- 0.5
+    return ( sum_t/(n*true_prob) - sum_c/(n*(1-true_prob)) )
+  } else if (type=="est2") {
+    est_prob <- sum(df$group=="treatment") / n
+    return ( sum_t/(n*est_prob) - sum_c/(n*(1-est_prob)) )
+  }
+}
+df <- create_rct_data(1000)
+est_tx_effect(df, "est1")
+est_tx_effect(df, "est2")
+
+# Chunk 2.4.1
+sim %<>% set_levels(
+  estimator = c("est1", "est2"),
+  num_patients = c(50, 200, 1000)
+)
+
+# Chunk 2.5.1
+sim %<>% set_script(function() {
+  df <- create_rct_data(L$num_patients)
+  est <- est_tx_effect(df, L$estimator)
+  return (list(
+    "est" = est,
+    "mean_t" = mean(df$outcome[df$group=="treatment"]),
+    "mean_c" = mean(df$outcome[df$group=="control"])
+  ))
+})
+
+# Chunk 2.6.1
+sim %<>% set_config(
+  num_sim = 100,
+  parallel = "outer",
+  n_cores = 2,
+  packages = c("ggplot2", "stringr")
+)
+
+# Chunk 2.7.1
+sim %<>% run()
+
+# Chunk 2.8.1
+sim %>% summarize(
+  list(stat="bias", truth=-7, estimate="est"),
+  list(stat="mse", truth=-7, estimate="est")
+)
+
+# Chunk 2.8.2
+head(sim$results)
+
+# Chunk 2.9.1
+sim %<>% set_config(num_sim = 200)
+sim %<>% set_levels(
+  estimator = c("est1", "est2"),
+  num_patients = c(20, 50, 200, 1000)
+)
+
+# Chunk 2.9.2
+sim %<>% update_sim()
+
+# Chunk 2.9.3
+sim %>% summarize(
+  list(stat="bias", truth=-7, estimate="est"),
+  list(stat="mse", truth=-7, estimate="est")
+)
+
 ## Section 3: Parallelization
+## The code in this section does not produce any results.
 
 ## Section 4: Advanced functionality
 
@@ -32,7 +129,7 @@ sim %<>% set_script(function() {
 })
 sim %<>% run()
 sim %>% summarize(
-  list(stat = "mse", name="lambda_mse", estimate="lambda_hat", truth=5), 
+  list(stat = "mse", name="lambda_mse", estimate="lambda_hat", truth=5),
   mc_se = TRUE)
 
 ## Appendix A: Simulation-based power calculation
@@ -53,7 +150,7 @@ create_rct_data(n=3, mu_0=3, mu_1=4, sigma_0=0.1, sigma_1=0.1)
 run_test <- function(data) {
   test_result <- t.test(outcome~group, data=data)
   return(as.integer(test_result$p.value<0.05))
-} 
+}
 
 # Chunk A.3 (no output)
 sim %<>% set_script(function() {
